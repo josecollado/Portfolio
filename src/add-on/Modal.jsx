49d57ea30461d727ef80f3a943f6b497d1b01a62
Jose Collado <jcoll1020@gmail.com>
2021-06-20 23:40:46 -0700

    modal styling completed

--- src/add-on/Modal.jsx
+++ src/add-on/Modal.jsx
@@ -1,19 +1,24 @@
-import { Modal } from '@material-ui/core';
+import { Modal, TextField, Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import React, { useState } from 'react';
 import AlternateEmailIcon from '@material-ui/icons/AlternateEmail';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import SendIcon from '@material-ui/icons/Send';
+
 
   
 
 const useStyles = makeStyles((theme) => ({
   paper: {
     position: 'absolute',
-    width: 400,
+    width: 750,
     backgroundColor: theme.palette.background.paper,
     border: '2px solid #000',
-    borderRadius: '10px',
-    boxShadow: theme.shadows[5],
+    borderRadius: '15px',
+    boxShadow: theme.shadows[3],
     padding: theme.spacing(2, 4, 3),
+    display: 'flex',
+    margin: 'auto'
   },
 }));
 
@@ -21,13 +26,64 @@ const useStyles = makeStyles((theme) => ({
 export const ContactModal = ({open, handleClose}) => {
     const classes = useStyles();
     const [modalStyle] = useState({margin: 'auto'});
+    const [messageValue, setMsgValue] = useState('')
+    const [nameValue, setNameValue] = useState('')
+    const [emailValue, setEmailValue] = useState('')
+    const handleMSGValue =(e) =>{
+      const value = e.target.value
+      return setMsgValue(value)
+    }
+    const handleEmailValue =(e)=>{
+      const value = e.target.value
+      return setEmailValue(value)
+    }
+    const handleNameValue=(e)=>{
+      const value = e.target.value
+      return setNameValue(value)
+    }
 
   const body = (
     <div style={modalStyle} className={classes.paper}>
-      <div className='flex mb-7 place-content-center border-black border-solid border-2 '>
-         <AlternateEmailIcon fontSize='medium'/>: <a href="mailto:">Email</a> 
+      <div className='flex flex-col w-3/4 main place-items-center py-10 '>
+      <div className='w-full mt-7 text-center text-md  '>
+        Want to get in touch? 
+        <br/>
+        Send me a message
+        <br/>
+        through E-mail, LinkedIn,
+        <br/>
+         or on my site.
+        <br/>
+        Happy to connect and talk more!
       </div>
-      <div className='flex place-content-center border-black border-solid border-2 '>box 2</div>
+         <div className='p-3 text-center py-10'>
+          
+         <AlternateEmailIcon fontSize='default' className='' />: <a href="mailto:" className=''>Email</a> 
+         </div>
+         <div className='pt-3 w-full text-center'>
+
+         <LinkedInIcon fontSize='default' className=''/> <a className='' href='https://www.linkedin.com/in/jose-collado/' target='_blank'>linkedin.com/in/jose-collado</a>
+         </div>
+      </div>
+      <div className='p-10 flex flex-col space-y-4 w-full place-content-center p-3 '>
+      <TextField onChange={handleNameValue} id="outlined-size-small 1" label="Name" variant="outlined" className='w-56'/>
+      <TextField onChange={handleEmailValue} id="outlined-size-small 2" label="E-mail" variant="outlined" className='w-64' />
+      <TextField
+          id="outlined-multiline-static"
+          label="Message.."
+          multiline
+          rows={5}
+          variant="outlined"
+          className=' w-80 '
+          onChange={handleMSGValue}
+        />
+        <div className=''>
+
+        <Button variant='outlined' className='w-full'><SendIcon/></Button>
+        </div>
+        
+
+      </div>
       
     </div>
   );

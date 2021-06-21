760da57874843e5a1ddfb973f04ff19492272f68
Jose Collado <jcoll1020@gmail.com>
2021-06-21 12:20:19 -0700

    working on view function

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import MainContent from '../assets/MainContent';
 import Lottie from 'react-lottie';
 import codeDesk from '../lotties/code-desk.json';
+
+
+
 
 const Main = () => {
   const defaultOptions = {
@@ -12,12 +15,12 @@ const Main = () => {
       preserveAspectRatio: 'xMidYMid slice',
     },
   };
+ 
   return (
     <div className='min-h-screen relative'>
-    <div className='  absolute'>
-
-      <MainContent />
-    </div>
+      <div className='  absolute'>
+        <MainContent />
+      </div>
       <div className=' z-10 w-1/2 right-0 float-right min-h-screen  pt-48 '>
         <Lottie options={defaultOptions} height={600} width={600} />
       </div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,40 +1,82 @@
-import React,{useState} from 'react'
-import logo from '../assets/images/logo1.png'
-import Links from '../assets/Links'
+import React, { useState } from 'react';
+import logo from '../assets/images/logo1.png';
+import Links from '../assets/Links';
 import PersonSharpIcon from '@material-ui/icons/PersonSharp';
 import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
 import ContactMailRoundedIcon from '@material-ui/icons/ContactMailRounded';
 import PermMediaRoundedIcon from '@material-ui/icons/PermMediaRounded';
-import {ContactModal} from '../add-on/Modal'
+import { ContactModal } from '../add-on/Modal';
+
+
+
 
 const Sidebar = () => {
-    const [open, setOpen] = useState(false)
-    const handleOpen = () => setOpen(true)
-    const handleClose = () => setOpen(false)
-    return (
-        <div className='flex flex-col min-h-full w-18'>
-            <div className='logo object-fill h-32 w-32 -mx-4  -mt-4 top-0 place-items-center mb-auto'>
-                <img src={logo} title='Logo'/>
-            </div>
-            <div className='flex flex-col place-items-center space-y-20'> 
-                <a href='#' className='object-fill h-9 w-9 ' title='Home Page'>
-                    <HomeRoundedIcon fontSize='large' />
-                 </a>
-                <a href='#' className='object-fill h-9 w-9' title='About Me'>
-                    <PersonSharpIcon fontSize='large' /> 
-                </a>
-                <a href='#' className='object-fill h-9 w-9' title='Projects'>
-                    <PermMediaRoundedIcon fontSize='large' />
-                </a>
-                
-                <ContactMailRoundedIcon fontSize='large' type='button' className='contact object-fill h-9 w-9' onClick={handleOpen} titleAccess='Contact Me' />
-                <ContactModal open={open} handleClose={handleClose} />
-            </div>
-            <Links />
-        
-        </div>
+    const [projectsView, setProjectsView] = useState(false);
+    const [homeView, setHomeView] = useState(false);
+    const [aboutView, setAboutView] = useState(false);
 
-    )
-}
+  const [open, setOpen] = useState(false);
 
-export default Sidebar
+  const handleClick = (value) => {
+    if (value === 'home') {
+      setProjectsView(false);
+      setAboutView(false);
+      setHomeView(true);
+    } else if (value === 'user') {
+      setProjectsView(false);
+      setAboutView(true);
+      setHomeView(false);
+    } else if (value === 'projects') {
+      setProjectsView(true);
+      setAboutView(false);
+      setHomeView(false);
+    } else {
+      setProjectsView(false);
+      setAboutView(false);
+      setHomeView(false);
+    }
+  };
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  return (
+    <div className='flex flex-col min-h-full w-18'>
+      <div className='logo object-fill h-32 w-32 -mx-4  -mt-4 top-0 place-items-center mb-auto'>
+        <img src={logo} title='Logo' />
+      </div>
+      <div className='flex flex-col place-items-center space-y-20'>
+        <HomeRoundedIcon
+          fontSize='large'
+          className='sidebar object-fill h-9 w-9 contact'
+          titleAccess='Home Page'
+          onClick={() => handleClick('home')}
+        />
+        <PersonSharpIcon
+          fontSize='large'
+          className='sidebar object-fill h-9 w-9'
+          titleAccess='About Me'
+          onClick={() => handleClick('user')}
+        />
+        <PermMediaRoundedIcon
+          fontSize='large'
+          className='sidebar object-fill h-9 w-9'
+          titleAccess='Projects'
+          onClick={() => handleClick('projects')}
+        />
+
+        <ContactMailRoundedIcon
+          fontSize='large'
+          type='button'
+          className='sidebar object-fill h-9 w-9'
+          onClick={handleOpen}
+          titleAccess='Contact Me'
+        />
+        <ContactModal open={open} handleClose={handleClose} />
+      </div>
+
+      <Links />
+    </div>
+  );
+};
+
+export default Sidebar;

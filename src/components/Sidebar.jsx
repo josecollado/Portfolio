6f9c96137d19d6d8064d198fed20a6fc2c5a3944
Jose Collado <jcoll1020@gmail.com>
2021-08-29 14:52:43 -0400

    Changed logo

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import logo from '../assets/images/logo1.png';
+import logo from '../assets/images/logo-main.png';
 import Links from '../assets/Links';
 import PersonSharpIcon from '@material-ui/icons/PersonSharp';
 import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
@@ -40,7 +40,7 @@ const Sidebar = ({
   const handleClose = () => setOpen(false);
   return (
     <div className='side-nav flex flex-col h-full w-18'>
-      <div className='logo object-fill h-32 w-32 -mx-4  -mt-4 top-0 place-items-center mb-auto sidebar'>
+      <div className='logo object-fill h-32 w-32 -mx-4 top-0 place-items-center mb-auto sidebar'>
         <img src={logo} title='Logo' onClick={() => handleClick('home')} />
       </div>
       <div className='flex flex-col place-items-center lg:space-y-20 md:space-y-14 '>

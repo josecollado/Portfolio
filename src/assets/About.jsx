cdc22ffc7e1bae40b8a2a3cc099918db940abca2
Jose Collado <jcoll1020@gmail.com>
2022-01-06 01:54:21 -0500

    working on about me page functions and styling

--- src/assets/About.jsx
+++ src/assets/About.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import LongAboutMe from './Content/LongAboutMe';
 
 const AboutMe = () => {
   return (
     <div className=' about min-h-screen relative '>
-      <div className=' border-black border-solid border-2 '>text box</div>
-      <div className=' border-black border-solid border-2 z-10 '>image box </div>
+      <div className=' border-black border-solid border-2 z-10 '>
+        image box{' '}
+      </div>
+      <div className='about-me-box bg-blueGray-300 rounded-xl border-black border-solid border-2  md:mx-24 md:my-24  overflow-y-scroll '>
+        <div className=' p-5 '>
+          <LongAboutMe />
+        </div>
+      </div>
     </div>
   );
 };
--- src/assets/Content/LongAboutMe.jsx
+++ src/assets/Content/LongAboutMe.jsx
@@ -1,14 +1,41 @@
-import React from 'react'
+import React from 'react';
 
-
-
-const LongAboutMe = () =>{
-    return (
-        <div>
-            hello long about me
-        </div>
-    )
-}
-
+const LongAboutMe = () => {
+  return (
+    <div>
+      Growing up in New York City, Jose loved many things, but his top 3 were
+      always ; Technology, Cars and Art. He started his career working as a bike
+      messenger, which allowed him to meet a variety of people. He was inspired
+      to work on his future and his passion, but just like any other 18 year
+      old, he didn’t know where to start. As his search began, he looked back to
+      his love for cars and motorcycles and how he could relate that to
+      technology. Jose wasn’t a top notch mechanic, especially with YouTube and
+      Google as his main tool, but he knew the responsibility of an engine is
+      the same as it is with networks. Technology can work without a network,
+      but what is tech without the power of the internet? So from being a
+      mechanic, he decided to venture out and work on one of the most important
+      pieces of technology, networks. Graduating from DeVry University, he
+      earned his degree in Network Communications and Management, where he
+      learned how to build and maintain network systems with Cisco routers and
+      switches. Before he could step into the industry of networking, he learned
+      about programming and engineering, and realized that there wasn’t much
+      creativity with building and maintaining networks. Since then, he has
+      connected with people in the community and learned what challenges they
+      faced on a daily basis. This intrigued him so much that he began
+      collaborating on a few projects and then ventured out into the world of
+      programming and development. Jose began using resources like FreeCodeCamp,
+      Codecademy, and TeamTreehouse. He loves problem solving, being creative
+      and seeing people happy with his work. After finding some projects to work
+      on from friends, he met a real software engineer and had to collaborate on
+      a project together. He noticed that he was lacking a lot of skills that
+      were needed in today’s industry, so instead of ending up in tutorial hell,
+      he went to a bootcamp. Hack Reactor took him in and helped him gain the
+      skills he needed to be a better engineer . He was able to find out his
+      weaknesses and strengths and how to apply them to any team or project he
+      is currently working on. Till this day, Jose is committed to being the
+      best he can be, one line of code at a time.
+    </div>
+  );
+};
 
 export default LongAboutMe;

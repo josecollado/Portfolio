694336740ed403e4b00def5ddb0fd7078731de7c
Jose Collado <jcoll1020@gmail.com>
2022-01-10 19:23:36 -0500

    completed styling and functionality with about me page

--- src/assets/About.jsx
+++ src/assets/About.jsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import LongAboutMe from './Content/LongAboutMe';
+import ShortAboutMe from './Content/ShortAboutMe';
 import { Button } from '@material-ui/core';
+import zoomedPic from './images/zoomedPic.png'
+import ProPic from './images/proPic.png'
 
 const AboutMe = () => {
+  const [button, setButtonClicked] = useState(true)
   return (
-    <div className=' about min-h-screen relative  '>
-      <div className=' border-black border-solid border-2 z-10  absolute ml-5 md:h-48 md:w-48 md:-mt-10 '>
-        image box{' '}
+    <div className=' about max-h-screen mb-72 relative  '>
+      <div className='  z-10 absolute ml-5 md:h-48 md:w-48 md:-mt-10 '>
+        <img src={ProPic} alt='Profile Picture' className=' rounded-xl' />
       </div>
-      <div className='about-me-box z-0 relative bg-blueGray-300 rounded-xl  md:mx-24 md:mt-40 overflow-y-scroll '>
-        <div className='flex place-content-center pt-4 pb-5'>
-        <Button variant="outlined">TL;DR</Button>
+      <div className='about-me-box z-0 relative bg-blueGray-300 rounded-xl  md:mx-24 md:mt-36 overflow-y-scroll '>
+        <div className='flex place-content-center pt-8 pb-2'>
+        <Button variant="outlined" size='small' onClick={() => !button ? setButtonClicked(true) : setButtonClicked(false) }>{button ? 'TL;DR':'Longer Version' }</Button>
         </div>
+        {
+          button
+          ?
           <LongAboutMe />
+          :
+          <ShortAboutMe />
+        }
       </div>
     </div>
   );
--- src/assets/Content/ShortAboutMe.jsx
+++ src/assets/Content/ShortAboutMe.jsx
@@ -4,8 +4,10 @@ import React from 'react'
 
 const ShortAboutMe = () =>{
     return (
-        <div>
-            hello short about me
+        <div className='p-8 mt-24  lg:text-lg  '>
+            <p>
+                Just a guy from New York City, that loves Technology and building things, whether it's cars or applications, he will get it done. 
+            </p>
         </div>
     )
 }

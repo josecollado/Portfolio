dbf9060747954464183127e44108df380076af68
Jose Collado <jcoll1020@gmail.com>
2022-01-20 02:32:44 -0500

    dont know what happened with last commit message but working on projects page now

--- src/assets/Projects.jsx
+++ src/assets/Projects.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import Lottie from 'react-lottie';
+import programmer from '../lotties/programmer.json';
 
 const Projects = () =>{
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: programmer,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
     return (
         <div className='projects min-h-screen relative '>
-        <div className=' flex flex-col  border-black border-solid border-2'>hello</div>
+        <div className=' flex flex-col  border-black border-solid border-2  float-left mt-52 pl-20'>
+        <Lottie options={defaultOptions} height={550} width={550} />
+        </div>
       </div>
     )
 }
